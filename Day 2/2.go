2ab7b4a27d0d5f098711923ef0ce4040728482d5
James Winegar <james.m.york-winegar@accenture.com>
2018-12-02 14:59:02 -0600

    finish day 2

--- Day 2/2.go
+++ Day 2/2.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
-	"strings"
-)
-
-package main
-
-import (
-"bufio"
-"fmt"
-"os"
-"strings"
 )
 
 func check(e error) {
@@ -22,40 +13,66 @@ func check(e error) {
 	}
 }
 
-
-func main() {
-	var id_array []string
-	var twoCount, threeCount = 0,0
-	var twoCounter, threeCounter = 0, 0
-	var characterMap map[string]int
-	characterMap = make(map[string]int)
-
-	file, err := os.Open("Day 2/input.txt")
-	check(err)
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
 	defer file.Close()
+
+	var lines []string
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		id := scanner.Text()
-		id_array = strings.Split(id, "")
-		for _, character := range id_array {
-			characterMap[character] = characterMap[character] + 1
-		}
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
 
-		twoCounter, threeCounter = 0, 0
-		for _, v := range characterMap {
-			if v == 2 && twoCounter == 0 {
-				twoCount++
-				twoCounter++
-			}
-			if v == 3 && threeCounter == 0 {
-				threeCount++
-				threeCounter++
-			}
-		}
+func hammingDistance(s1 string, s2 string) (distance int, err error) {
+	r1 := []rune(s1)
+	r2 := []rune(s2)
 
-		for k := range characterMap {
-			delete(characterMap, k)
+	if len(r1) != len(r2) {
+		err = errors.New("Hamming distance of different sized strings.")
+		return
+	}
+
+	for i, v := range r1 {
+		if r2[i] != v {
+			distance += 1
+		}
+	}
+	return
+}
+
+func matchingCharacters(s1 string, s2 string) (result string, err error) {
+	if len(s1) != len(s2) {
+		err = errors.New("Cannot compare elements of  different sized strings.")
+		return
+	}
+
+	for i, char := range s1 {
+		if s1[i] == s2[i] {
+			result += string(char)
+		}
+	}
+	return
+}
+
+func main() {
+	lines, err := readLines("Day 2/input.txt")
+	check(err)
+
+	for i, line1 := range lines {
+		for _, line2 := range lines[i+1:] {
+			distance, err := hammingDistance(line1, line2)
+			check(err)
+			if distance == 1 {
+				result, err := matchingCharacters(line1, line2)
+				check(err)
+				fmt.Println(result)
+			}
 		}
 	}
 }
